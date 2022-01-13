c76b5e8ad988b91691681bff8f875cd0779d5427
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2022-01-13 23:49:14 +0100

    Web static files

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A module for web application deployment with Fabric."""
+from fabric.api import *
+import os
+from datetime import datetime
+
+
+def do_pack():
+    """archives the static file"""
+    try:
+        filepath = "versions/web_static_" + datetime.now().\
+            strftime("%Y%m%d%H%M%S") + ".tgz"
+        local("mkdir -p versions")
+        local("tar -zcvf versions/web_static_$(date +%Y%m%d%H%M%S).tgz\
+    web_static")
+        print("web_static packed: {} -> {}".
+              format(filepath, os.path.getsize(filepath)))
+    except BaseException:
+        return None
